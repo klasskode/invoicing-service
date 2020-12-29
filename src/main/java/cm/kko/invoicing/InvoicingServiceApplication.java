10208a975d1a58a959a00e8eff85ef2f55655989
Brice amk <ambiandji@gmail.com>
2020-12-29 19:01:22 +0100

    Initial commit

---
+++ src/main/java/cm/kko/invoicing/InvoicingServiceApplication.java
@@ -0,0 +1,13 @@
+package cm.kko.invoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cm/kko/invoicing/InvoicingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.kko.invoicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
